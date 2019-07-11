9b38dc03bc47b0a3733520c6a4ecd3016d23276e
Ang Sherpa <angsherpa232@gmail.com>
2019-07-11 11:27:21 +0200

    ribbon text centered (#12)
    * ribbon text centered.
    
    * ribbon styled.
    
    * ribbon styled with light color.

--- src/components/Ribbon.js
+++ src/components/Ribbon.js
@@ -3,21 +3,25 @@ import propTypes from 'prop-types';
 
 const styles = {
   position: 'fixed',
-  bottom: '20px',
-  backgroundColor: '#8b0e00',
-  right: '-35px',
+  bottom: '35px',
+  backgroundColor: '#b3372b',
+  right: '-51px',
   transform: 'rotate(-45deg)',
   zIndex: '10',
+  lineHeight: '24px',
+  opacity: '0.8',
+  boxShadow: 'rgba(123, 85, 85, 0.85) 0px 0px 10px 0px',
 };
 
 const textStyle = {
   border: '2px solid white',
   color: 'white',
-  fontSize: '14px',
-  fontFamily: 'Sans-Serif',
-  fontWeight: 'bold',
-  margin: '2px',
+  fontSize: '1.3rem',
+  fontWeight: 'bolder',
   padding: '0 70px',
+  margin: '2px',
+  textAlign: 'center',
+  letterSpacing: '3px',
 };
 
 const Ribbon = ({ label }) => (
