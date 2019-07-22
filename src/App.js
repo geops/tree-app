43ee06297bf5dde8334534805381844b49ccedf7
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2019-07-22 14:14:54 +0200

    Implement mobile navigation menu.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import useWindowSize from '@rehooks/window-size';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import 'semantic-ui-css/semantic.min.css';
-import { Divider, Grid, Header } from 'semantic-ui-react';
+import { Divider, Header, Icon, Menu } from 'semantic-ui-react';
 
 import Recommendation from './components/Recommendation';
 import Ribbon from './components/Ribbon';
@@ -9,16 +10,37 @@ import Ribbon from './components/Ribbon';
 import Map from './components/Map';
 
 function App() {
+  const [page, setPage] = useState('map');
   const { t } = useTranslation();
+  const isMobile = useWindowSize().innerWidth < 768;
+  const offset = page === 'map' ? 0 : -100;
   document.title = t('app.title');
-
   return (
-    <Grid columns={2}>
-      <Grid.Row style={{ paddingBottom: 0 }}>
-        <Grid.Column>
+    <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <div
+        style={{
+          flexGrow: 1,
+          display: 'flex',
+          width: isMobile ? '200vw' : '100vw',
+          transform: isMobile ? `translateX(${offset}vw)` : '',
+          transition: 'transform ease-out 0.2s',
+          willChange: 'transform',
+          overflow: 'hidden',
+        }}
+      >
+        <div
+          style={{ width: isMobile ? '100vw' : '50vw', position: 'relative' }}
+        >
           <Map />
-        </Grid.Column>
-        <Grid.Column>
+        </div>
+        <div
+          style={{
+            width: isMobile ? '100vw' : '50vw',
+            position: 'relative',
+            padding: '20px',
+            overflow: 'scroll',
+          }}
+        >
           <Divider hidden />
           <Header size="huge" textAlign="center" color="#055418">
             {t('app.title')}
@@ -26,10 +48,35 @@ function App() {
           <Divider hidden />
           <Recommendation />
           <Divider hidden />
-          <Ribbon label={t('app.ribbon')} />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+        </div>
+      </div>
+      {isMobile && (
+        <Menu
+          fluid
+          widths={2}
+          icon="labeled"
+          style={{ borderRadius: 0, margin: 0, flexShrink: 0 }}
+        >
+          <Menu.Item
+            active={page === 'map'}
+            onClick={() => setPage('map')}
+            style={{ borderRadius: 0 }}
+          >
+            <Icon name="map" />
+            {t('app.map')}
+          </Menu.Item>
+          <Menu.Item
+            active={page === 'recommendation'}
+            onClick={() => setPage('recommendation')}
+            style={{ borderRadius: 0 }}
+          >
+            <Icon name="tree" />
+            {t('app.recommendation')}
+          </Menu.Item>
+        </Menu>
+      )}
+      <Ribbon label={t('app.ribbon')} />
+    </div>
   );
 }
 
