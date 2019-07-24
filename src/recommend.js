0d6a4305c26ce43d2bbdd50b0b7ee1714cdb32d6
Ang Sherpa <ang.dawa.sherpa@geops.de>
2019-07-24 13:53:43 +0200

    recommendation function extended for future flag (#29)
    * test added to handle invalid forestType.spread operator for arguments removed.
    
    * future flag added along with running test.
    
    * future flag boolean type test running.
    
    * maintained uniformity in letter case for test and main recommendation library.

--- src/recommend.js
+++ src/recommend.js
@@ -1,34 +1,53 @@
 import recommendations from '../data/recommendations.json';
+import types from '../data/types.json';
 
 function recommendTreeSpecies(forestType) {
+  if (types.forestType.find(v => v.code === forestType) === undefined) {
+    throw new Error(`${forestType} is not a valid forest type`);
+  }
   const [, treeSpecies] = Object.entries(recommendations).find(
     t => t[0] === forestType,
   );
   return treeSpecies;
 }
 
-function concatTreeSpecies(treeSp1, treeSp2) {
-  return Array.from(new Set([...treeSp1, ...treeSp2]));
-}
+const concatTreeSpecies = (treeSp1, treeSp2) =>
+  Array.from(new Set([...treeSp1, ...treeSp2]));
 
-function recommend(...forestType) {
-  let result;
-  if (forestType.length === 0) {
+const filterTreeSpecies = (treeSp1, treeSp2) =>
+  treeSp2.filter(tree => !treeSp1.includes(tree));
+
+function recommend(forestType1, forestType2, future) {
+  if (!forestType1) {
     throw new Error(
       `at least one forest type is required to get the recommendation of tree species`,
     );
-  } else if (forestType.length === 1) {
-    result = recommendTreeSpecies(forestType[0]);
-  } else if (forestType.length === 2) {
-    const { one, two, three } = recommendTreeSpecies(forestType[0]);
+  }
+
+  if (future && typeof future !== 'boolean') {
+    throw new Error(`expected boolean type for future flag`);
+  }
+
+  let result = recommendTreeSpecies(forestType1);
+
+  const { one, two, three } = result;
+  if (forestType2) {
     const { one: one2, two: two2, three: three2 } = recommendTreeSpecies(
-      forestType[1],
+      forestType2,
     );
-    result = {
-      one: concatTreeSpecies(one, one2),
-      two: concatTreeSpecies(two, two2),
-      three: concatTreeSpecies(three, three2),
-    };
+    if (future) {
+      result = {
+        one: filterTreeSpecies(one, one2),
+        two: filterTreeSpecies(two, two2),
+        three: filterTreeSpecies(three, three2),
+      };
+    } else {
+      result = {
+        one: concatTreeSpecies(one, one2),
+        two: concatTreeSpecies(two, two2),
+        three: concatTreeSpecies(three, three2),
+      };
+    }
   }
 
   return result;
--- src/recommend.test.js
+++ src/recommend.test.js
@@ -1,34 +1,56 @@
 const { recommend } = require('../');
 
-test('valid single forestType recommendations', () => {
-  expect(recommend('60')).toStrictEqual({
-    one: [302800],
-    three: [800, 25400, 60400, 60500, 227200, 363700],
-    two: [402300],
+describe('Test for input values', () => {
+  test('at least one forest type required', () => {
+    expect(() => recommend()).toThrowError(
+      `at least one forest type is required to get the recommendation of tree species`,
+    );
+  });
+
+  test('invalid forestType1', () => {
+    expect(() => recommend('963D')).toThrowError(
+      `963D is not a valid forest type`,
+    );
+  });
+
+  test('invalid forestType2', () => {
+    expect(() => recommend('60', '963D')).toThrowError(
+      `963D is not a valid forest type`,
+    );
+  });
+
+  test('invalid data type for future flag', () => {
+    expect(() => recommend('60', '50', 56)).toThrowError(
+      `expected boolean type for future flag`,
+    );
   });
 });
 
-test('valid multiple forestType recommendations', () => {
-  expect(recommend('60', '50')).toStrictEqual({
-    one: [302800, 100],
-    two: [402300, 800],
-    three: [
-      800,
-      25400,
-      60400,
-      60500,
-      227200,
-      363700,
-      25300,
-      174200,
-      317500,
-      402200,
-    ],
+describe('Test for output values', () => {
+  test('valid single forest type recommendations', () => {
+    expect(recommend('60')).toStrictEqual({
+      one: [302800],
+      three: [800, 25400, 60400, 60500, 227200, 363700],
+      two: [402300],
+    });
   });
-});
 
-test('at least one forest Type required', () => {
-  expect(() => recommend()).toThrowError(
-    `at least one forest type is required to get the recommendation of tree species`,
-  );
+  test('valid multiple forest type recommendations', () => {
+    expect(recommend('60', '50')).toStrictEqual({
+      one: [302800, 100],
+      two: [402300, 800],
+      three: [
+        800,
+        25400,
+        60400,
+        60500,
+        227200,
+        363700,
+        25300,
+        174200,
+        317500,
+        402200,
+      ],
+    });
+  });
 });
