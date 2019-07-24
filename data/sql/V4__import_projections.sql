4a57d55b4e8a0f4045afd0d215383f21f95e2ed6
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2019-07-24 11:45:34 +0200

    Remove unused columns reliktareal and nebenareal from CSV file.

--- data/sql/V4__import_projections.sql
+++ data/sql/V4__import_projections.sql
@@ -1,4 +1,4 @@
-CREATE TABLE projections_import (regions TEXT, heightLevel TEXT, forestType TEXT,slope TEXT,condition TEXT,targets TEXT,tannenareal TEXT,standortsregion TEXT,reliktareal TEXT,nebenareal TEXT, relief TEXT,targetHeight TEXT);
+CREATE TABLE projections_import (regions TEXT, heightLevel TEXT, forestType TEXT, slope TEXT, condition TEXT, targets TEXT, tannenareal TEXT, standortsregion TEXT, relief TEXT, targetHeight TEXT);
 
 COPY projections_import
 FROM '/data/projections.csv'
