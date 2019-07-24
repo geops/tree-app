36d68fbee626ec4675a1e776969be8898b21d52f
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2019-07-24 17:38:51 +0200

    Add forest types for lucerne.

--- data/spatial/3-tile.sh
+++ data/spatial/3-tile.sh
@@ -8,9 +8,10 @@ _tile "altitudinal_zones_1995"
 _tile "altitudinal_zones_2085_dry"
 _tile "altitudinal_zones_2085_less_dry"
 _tile "forest_ecoregions"
+_tile "forest_types"
 _tile "silver_fir_areas"
 
 echo "Joining tiles ..."
 cd /data/spatial/tiles
-tile-join --force --maximum-zoom=10 --no-tile-size-limit -e "tree" altitudinal_zones_1995.mbtiles altitudinal_zones_2085_dry.mbtiles altitudinal_zones_2085_less_dry.mbtiles forest_ecoregions.mbtiles silver_fir_areas.mbtiles
-rm altitudinal_zones_1995.mbtiles altitudinal_zones_2085_dry.mbtiles altitudinal_zones_2085_less_dry.mbtiles forest_ecoregions.mbtiles silver_fir_areas.mbtiles
+tile-join --force --maximum-zoom=10 --no-tile-size-limit -e "tree" altitudinal_zones_1995.mbtiles altitudinal_zones_2085_dry.mbtiles altitudinal_zones_2085_less_dry.mbtiles forest_ecoregions.mbtiles forest_types.mbtiles silver_fir_areas.mbtiles
+rm altitudinal_zones_1995.mbtiles altitudinal_zones_2085_dry.mbtiles altitudinal_zones_2085_less_dry.mbtiles forest_ecoregions.mbtiles forest_types.mbtiles silver_fir_areas.mbtiles
