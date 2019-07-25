e2593b67d7ea244f377e3048bca7d9b30d27b2fa
Ang Sherpa <angsherpa232@gmail.com>
2019-07-25 19:38:28 +0200

    onTabChange callback added to tab change behavior.

--- src/components/ProjectionResult.js
+++ src/components/ProjectionResult.js
@@ -1,5 +1,5 @@
 import { translate } from '@geops/tree-lib';
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { Divider, Form, Header, Tab } from 'semantic-ui-react';
@@ -7,45 +7,70 @@ import { Divider, Form, Header, Tab } from 'semantic-ui-react';
 import Recommendation from './Recommendation';
 
 function ProjectionResult() {
-  const { projectionLocation, mapLocation } = useSelector(state => ({
+  const { projectionLocation, location } = useSelector(state => ({
     projectionLocation: state.projectionLocation,
-    mapLocation: state.mapLocation,
+    location: state.location,
   }));
   const { t, i18n } = useTranslation();
+
+  const [future, setFuture] = useState(false);
 
   const panes = [
     {
       menuItem: t('tab.scenario1'),
-      render: () => <Tab.Pane>Coming soon...</Tab.Pane>,
+      render: () => (
+        <Tab.Pane>
+          {' '}
+          <Header>
+            {`${projectionLocation.forestType} - ${translate(
+              'forestType',
+              projectionLocation.forestType,
+              i18n.language,
+            )}`}
+          </Header>
+          <Divider hidden />
+          <Recommendation
+            forestTypeToday={location.forestType}
+            forestTypeFuture={projectionLocation.forestType}
+            future={future}
+          />
+        </Tab.Pane>
+      ),
     },
     {
       menuItem: t('tab.scenario2'),
       render: () => (
         <Tab.Pane>
           <Header>
-            {projectionLocation.forestType}
-            {' - '}
-            {translate(
+            {`${projectionLocation.forestType} - ${translate(
               'forestType',
               projectionLocation.forestType,
               i18n.language,
-            )}
+            )}`}
           </Header>
           <Form>
             <Form.Field>
-              <Form.Radio label="bereits heute mögliche Baumarten" checked />
+              <Form.Radio
+                label="bereits heute mögliche Baumarten"
+                name="radioGroup"
+                checked={future === false}
+                onChange={() => setFuture(false)}
+              />
             </Form.Field>
             <Form.Field>
               <Form.Radio
                 label="in Zukunft zusätzliche Baumarten"
-                checked={false}
+                name="radioGroup"
+                checked={future === true}
+                onChange={() => setFuture(true)}
               />
             </Form.Field>
           </Form>
           <Divider hidden />
           <Recommendation
-            forestTypeToday={mapLocation.forestType}
+            forestTypeToday={location.forestType}
             forestTypeFuture={projectionLocation.forestType}
+            future={future}
           />
         </Tab.Pane>
       ),
@@ -63,7 +88,13 @@ function ProjectionResult() {
           <Divider horizontal>
             <Header color="olive">{t('app.result')}</Header>
           </Divider>
-          <Tab panes={panes} defaultActiveIndex={1} />
+          <Tab
+            panes={panes}
+            defaultActiveIndex={1}
+            onTabChange={(e, data) =>
+              console.log(data.panes[data.activeIndex].menuItem)
+            }
+          />
         </>
       )}
     </>
