31fad2722ffc17d103995a6e4daf8b77053c2483
Ang Sherpa <angsherpa232@gmail.com>
2019-07-26 14:46:39 +0200

    tab code duplication removed by shifting the <Tab.pane> to recommendation component from projectionResult component

--- src/components/ProjectionResult.js
+++ src/components/ProjectionResult.js
@@ -1,8 +1,7 @@
-import { translate } from '@geops/tree-lib';
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector, useDispatch } from 'react-redux';
-import { Divider, Form, Header, Tab } from 'semantic-ui-react';
+import { Divider, Header, Tab } from 'semantic-ui-react';
 
 import { setRecommendationMode } from '../store/actions';
 import Recommendation from './Recommendation';
@@ -13,105 +12,42 @@ function ProjectionResult() {
     projectionLocation: state.projectionLocation,
     location: state.location,
   }));
-  const { t, i18n } = useTranslation();
-
-  const [future, setFuture] = useState(false);
+  const { t } = useTranslation();
 
   const panes = [
     {
       menuItem: t('recommendationMode.today'),
       recommendationMode: 'today',
       render: () => (
-        <Tab.Pane>
-          {' '}
-          <Header>
-            {`${location.forestType} - ${translate(
-              'forestType',
-              location.forestType,
-              i18n.language,
-            )}`}
-          </Header>
-          <Divider hidden />
-          <Recommendation forestTypeToday={location.forestType} />
-        </Tab.Pane>
+        <Recommendation
+          forestTypeToday={location.forestType}
+          forest={location.forestType}
+          todayFutureToggler={false}
+        />
       ),
     },
     {
       menuItem: t('recommendationMode.moderate'),
       recommendationMode: 'moderate',
       render: () => (
-        <Tab.Pane>
-          <Header>
-            {`${projectionLocation.forestType} - ${translate(
-              'forestType',
-              projectionLocation.forestType,
-              i18n.language,
-            )}`}
-          </Header>
-          <Form>
-            <Form.Field>
-              <Form.Radio
-                label="bereits heute mögliche Baumarten"
-                name="radioGroup"
-                checked={future === false}
-                onChange={() => setFuture(false)}
-              />
-            </Form.Field>
-            <Form.Field>
-              <Form.Radio
-                label="in Zukunft zusätzliche Baumarten"
-                name="radioGroup"
-                checked={future === true}
-                onChange={() => setFuture(true)}
-              />
-            </Form.Field>
-          </Form>
-          <Divider hidden />
-          <Recommendation
-            forestTypeToday={location.forestType}
-            forestTypeFuture={projectionLocation.forestType}
-            future={future}
-          />
-        </Tab.Pane>
+        <Recommendation
+          forestTypeToday={location.forestType}
+          forestTypeFuture={projectionLocation.forestType}
+          forest={projectionLocation.forestType}
+          todayFutureToggler
+        />
       ),
     },
     {
       menuItem: t('recommendationMode.extreme'),
       recommendationMode: 'extreme',
       render: () => (
-        <Tab.Pane>
-          <Header>
-            {`${projectionLocation.forestType} - ${translate(
-              'forestType',
-              projectionLocation.forestType,
-              i18n.language,
-            )}`}
-          </Header>
-          <Form>
-            <Form.Field>
-              <Form.Radio
-                label="bereits heute mögliche Baumarten"
-                name="radioGroup"
-                checked={future === false}
-                onChange={() => setFuture(false)}
-              />
-            </Form.Field>
-            <Form.Field>
-              <Form.Radio
-                label="in Zukunft zusätzliche Baumarten"
-                name="radioGroup"
-                checked={future === true}
-                onChange={() => setFuture(true)}
-              />
-            </Form.Field>
-          </Form>
-          <Divider hidden />
-          <Recommendation
-            forestTypeToday={location.forestType}
-            forestTypeFuture={projectionLocation.forestType}
-            future={future}
-          />
-        </Tab.Pane>
+        <Recommendation
+          forestTypeToday={location.forestType}
+          forestTypeFuture={projectionLocation.forestType}
+          forest={projectionLocation.forestType}
+          todayFutureToggler
+        />
       ),
     },
   ];
