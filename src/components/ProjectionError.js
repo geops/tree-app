c37c80b52ed2a5bd8f59441b11d98d8191b46531
Ang Sherpa <angsherpa232@gmail.com>
2019-07-31 15:27:22 +0200

    error message to notify no projection found removed as the same purpose is covered by forestType not found error.

--- src/components/ProjectionError.js
+++ src/components/ProjectionError.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { Message } from 'semantic-ui-react';
+import { Message, Grid } from 'semantic-ui-react';
 
 const requiredFieldsForProjection = [
   'forestEcoregion',
@@ -10,42 +10,30 @@ const requiredFieldsForProjection = [
 ];
 
 function ProjectionError() {
-  const { mapLocation, projectionMode, projectionLocation } = useSelector(
-    state => ({
-      mapLocation: state.mapLocation,
-      projectionMode: state.projectionMode,
-      projectionLocation: state.projectionLocation,
-    }),
-  );
+  const { mapLocation, projectionMode } = useSelector(state => ({
+    mapLocation: state.mapLocation,
+    projectionMode: state.projectionMode,
+    projectionLocation: state.projectionLocation,
+  }));
 
   const { t } = useTranslation();
   const missingFields = requiredFieldsForProjection
     .filter(field => mapLocation[field] === undefined)
     .map(field => t(`${field}.label`));
 
-  return (
-    projectionMode === 'map' && (
-      <>
-        {mapLocation.coordinate && missingFields.length >= 1 && (
-          <Message
-            error
-            header={t('errorMessage.incompleteLocationInput')}
-            list={missingFields}
-            size="large"
-          />
-        )}
-        {missingFields.length === 0 &&
-          mapLocation.forestType &&
-          projectionLocation.forestType === undefined && (
-            <Message negative>
-              <Message.Header>
-                {t('errorMessage.noProjectionFoundMessage')}
-              </Message.Header>
-            </Message>
-          )}
-      </>
-    )
-  );
+  return projectionMode === 'map' &&
+    mapLocation.coordinate &&
+    missingFields.length >= 1 ? (
+    <Grid columns={1} centered>
+      <Message
+        error
+        header={t('errorMessage.incompleteLocationInput')}
+        list={missingFields}
+        size="medium"
+        compact
+      />
+    </Grid>
+  ) : null;
 }
 
 export default ProjectionError;
