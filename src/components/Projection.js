b4cbf7a93dea57ba1db789ecd6ce049cbf1d34a5
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2019-08-05 13:36:21 +0200

    Move message about missing location input into ProjectionForm component.

--- src/components/Projection.js
+++ src/components/Projection.js
@@ -3,14 +3,12 @@ import React from 'react';
 import ProjectionForm from './ProjectionForm';
 import ProjectionMode from './ProjectionMode';
 import ProjectionResult from './ProjectionResult';
-import ProjectionError from './ProjectionError';
 
 function Projection() {
   return (
     <>
       <ProjectionMode />
       <ProjectionForm />
-      <ProjectionError />
       <ProjectionResult />
     </>
   );
--- src/components/ProjectionForm.js
+++ src/components/ProjectionForm.js
@@ -4,7 +4,7 @@ import { translate } from '@geops/tree-lib';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
-import { Form } from 'semantic-ui-react';
+import { Form, Label } from 'semantic-ui-react';
 
 import ChoiceButton from './ChoiceButton';
 import Dropdown from './Dropdown';
@@ -59,8 +59,13 @@ function ProjectionForm() {
                 ? t('forestType.hint')
                 : t('dropdownPlaceholder')
             }
-            value={location.forestType}
+            value={location.forestType || ''}
           />
+          {projectionMode === 'm' &&
+            location.coordinate &&
+            !location.forestType && (
+              <Label pointing>{t('error.incompleteLocationInput')}</Label>
+            )}
         </Form.Field>
       )}
       {projectionMode === 'f' && projectionOptions.forestEcoregion && (
