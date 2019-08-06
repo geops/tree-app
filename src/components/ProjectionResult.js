87ec5c21283d85bb75411527a7a4b4999ac64c9a
Ang Sherpa <ang.dawa.sherpa@geops.de>
2019-08-06 18:12:38 +0200

    Result Pointer removed and replaced by shadow (#59)
    * resultPointer removed replaced by shadow
    
    * Update box-shadow value
    
    Co-Authored-By: friedjoff <friedjoff.trautwein@geops.de>

--- src/components/ProjectionResult.js
+++ src/components/ProjectionResult.js
@@ -1,21 +1,17 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector, useDispatch } from 'react-redux';
 import { Tab } from 'semantic-ui-react';
-import useComponentSize from '@rehooks/component-size';
 
 import { setRecommendationMode } from '../store/actions';
 import Recommendation from './Recommendation';
 import styles from './ProjectionResult.module.css';
-import { ReactComponent as RecommendationPointer } from '../icons/recommendationPointer.svg';
 
 function ProjectionResult() {
   const dispatch = useDispatch();
   const projectionLocation = useSelector(state => state.projectionLocation);
   const { t } = useTranslation();
 
-  const ref = useRef(null);
-  const { height } = useComponentSize(ref);
   const panes = [
     {
       menuItem: t('recommendationMode.today'),
@@ -35,12 +31,7 @@ function ProjectionResult() {
   ];
 
   return projectionLocation.forestType && projectionLocation.altitudinalZone ? (
-    <div
-      ref={ref}
-      className={styles.container}
-      style={{ bottom: `${20 - height}px` }}
-    >
-      <RecommendationPointer className={styles.pointer} />
+    <div className={styles.container}>
       <Tab
         className={styles.tab}
         defaultActiveIndex={2}
