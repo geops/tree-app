5f6a195e3bb83762bd08b3814d439261511319a0
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2019-08-07 17:12:39 +0200

    Improve layout for mobile Safari.

--- src/components/ProjectionMode.js
+++ src/components/ProjectionMode.js
@@ -2,10 +2,11 @@ import { transform } from 'ol/proj';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
-import { Grid } from 'semantic-ui-react';
+import { Label } from 'semantic-ui-react';
 
 import Button from './Button';
 import { ReactComponent as ManualIcon } from '../icons/manual.svg';
+import { ReactComponent as MapPosition } from '../icons/mapPosition.svg';
 import { ReactComponent as MapIcon } from '../icons/map.svg';
 import { EPSG2056 } from '../map/projection';
 import { setProjectionMode } from '../store/actions';
@@ -25,31 +26,33 @@ function ProjectionMode() {
 
   const { t } = useTranslation();
   return (
-    <Grid columns="2" verticalAlign="middle" className={styles.grid}>
-      <Grid.Row>
-        <Grid.Column>
-          <Button
-            disabled
-            icon="point"
-            label={coordinate ? formatCoordinates(coordinate) : t('map.hint')}
-          />
-        </Grid.Column>
-        <Grid.Column textAlign="right">
-          <Button.Group>
-            <Button
-              active={projectionMode === 'm'}
-              content={<MapIcon fill="white" className={styles.icon} />}
-              onClick={() => dispatch(setProjectionMode('m'))}
-            />
-            <Button
-              active={projectionMode === 'f'}
-              content={<ManualIcon fill="white" className={styles.icon} />}
-              onClick={() => dispatch(setProjectionMode('f'))}
-            />
-          </Button.Group>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+    <div className={styles.row}>
+      <Button
+        disabled
+        color={projectionMode === 'm' ? 'black' : 'grey'}
+        icon={
+          // Reset size because OpenLayers map icon requires height and width.
+          <MapPosition height={null} width={null} className={styles.icon} />
+        }
+        label={
+          <Label color={projectionMode === 'm' ? 'black' : 'grey'}>
+            {coordinate ? formatCoordinates(coordinate) : t('map.hint')}
+          </Label>
+        }
+      />
+      <Button.Group className={styles.group}>
+        <Button
+          active={projectionMode === 'm'}
+          content={<MapIcon fill="white" className={styles.icon} />}
+          onClick={() => dispatch(setProjectionMode('m'))}
+        />
+        <Button
+          active={projectionMode === 'f'}
+          content={<ManualIcon fill="white" className={styles.icon} />}
+          onClick={() => dispatch(setProjectionMode('f'))}
+        />
+      </Button.Group>
+    </div>
   );
 }
 
