54606da0700d194ca7d8e0f925a39c7c5e03f7fb
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2019-11-18 15:13:03 +0100

    Remove useless help icon from ProjectionTab component.

--- src/components/ProjectionTab.js
+++ src/components/ProjectionTab.js
@@ -5,45 +5,34 @@ import { useTranslation } from 'react-i18next';
 import { Grid, Header, Tab } from 'semantic-ui-react';
 
 import TreeTypeList from './TreeTypeList';
-import { ReactComponent as InfoIcon } from '../icons/info.svg';
-import styles from './ProjectionTab.module.css';
 
 function ProjectionTab({ forestType }) {
   const treeTypes = useMemo(() => list(forestType), [forestType]);
   const { t } = useTranslation();
   return (
     <Tab.Pane>
-      <Grid columns={3} padded verticalAlign="middle">
+      <Grid columns={2} padded verticalAlign="middle">
         <Grid.Row>
-          <Grid.Column width={2} textAlign="center">
-            <InfoIcon fill="white" className={styles.infoIcon} />
-          </Grid.Column>
-          <Grid.Column width={6}>
+          <Grid.Column>
             <Header inverted>{t('projection.treeTypesOne')}</Header>
           </Grid.Column>
-          <Grid.Column width={8}>
+          <Grid.Column>
             <TreeTypeList codes={treeTypes[0]} />
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
-          <Grid.Column width={2} textAlign="center">
-            <InfoIcon fill="white" className={styles.infoIcon} />
-          </Grid.Column>
-          <Grid.Column width={6}>
+          <Grid.Column>
             <Header inverted>{t('projection.treeTypesTwo')}</Header>
           </Grid.Column>
-          <Grid.Column width={8}>
+          <Grid.Column>
             <TreeTypeList codes={treeTypes[1]} />
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
-          <Grid.Column width={2} textAlign="center">
-            <InfoIcon fill="white" className={styles.infoIcon} />
-          </Grid.Column>
-          <Grid.Column width={6}>
+          <Grid.Column>
             <Header inverted>{t('projection.treeTypesThree')}</Header>
           </Grid.Column>
-          <Grid.Column width={8}>
+          <Grid.Column>
             <TreeTypeList codes={treeTypes[2]} />
           </Grid.Column>
         </Grid.Row>
@@ -53,11 +42,7 @@ function ProjectionTab({ forestType }) {
 }
 
 ProjectionTab.propTypes = {
-  forestType: PropTypes.string,
-};
-
-ProjectionTab.defaultProps = {
-  forestType: '',
+  forestType: PropTypes.string.isRequired,
 };
 
 export default ProjectionTab;
