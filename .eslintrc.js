828e190e81303a5c97c567841aade89a685f4ecb
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2019-12-17 09:40:26 +0100

    Move ESLint config to Neutrino config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,3 @@
 const neutrino = require('neutrino');
 
-const config = neutrino().eslintrc();
-
-config.env.jest = true;
-config.extends.push('prettier');
-config.plugins.push('prettier');
-
-module.exports = config;
+module.exports = neutrino().eslintrc();
