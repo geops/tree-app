ff2a329fb59e2726fe3b4a2568b59aa1bdf90339
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-01-08 14:29:09 +0100

    Show messages about offline status.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { toast } from 'react-toastify';
+
 import App from './App';
+import i18n from './i18n';
 import * as serviceWorker from './serviceWorker';
+
+import 'react-toastify/dist/ReactToastify.css';
 import 'semantic-ui-css/semantic.min.css';
 
-import './i18n';
+toast.configure();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register({
+  onUpdate: () => toast(i18n.t('offline.update'), { type: toast.TYPE.INFO }),
+  onSuccess: () => toast(i18n.t('offline.success'), { type: toast.TYPE.INFO }),
+});
