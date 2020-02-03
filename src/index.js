e8097ea1593e08f265907f7d160da24cc01a8218
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-02-03 08:17:16 +0100

    feat: add version to default export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import { version } from '../package.json';
+
 export { default as info } from './info';
 export { default as list } from './list';
 export { default as project } from './project';
 export { default as recommend } from './recommend';
 
-export default null;
+export default { version };
