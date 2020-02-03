ca2a8f675e1fbfa236fb671c19730e00eb857131
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-02-03 08:05:07 +0100

    chore: fix lint-staged config by removing git add.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,6 @@ module.exports = {
   'src/**/*.js': [
     'yarn lint --fix',
     "prettier --write 'src/**/*.js'",
-    'git add',
     'yarn test --bail --findRelatedTests --watchAll=false',
   ],
 };
