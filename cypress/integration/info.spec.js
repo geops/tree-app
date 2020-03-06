8ea1fbfaf7a81dcb88854843bee3ec9d6699c536
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-03-06 08:05:29 +0100

    chore: fix element selection in integration test

--- cypress/integration/info.spec.js
+++ cypress/integration/info.spec.js
@@ -8,11 +8,11 @@ describe('Info', () => {
   });
 
   it('shows title for impressum', () => {
-    cy.get('.ui.accordion > :nth-last-child(2)').contains(info.impressumTitle);
+    cy.get('.ui.accordion > :nth-child(11)').contains(info.impressumTitle);
   });
 
   it('shows impressum after clicking on title', () => {
-    cy.get('.ui.accordion > :nth-last-child(2)').click();
-    cy.get('.ui.accordion > :nth-last-child(1)').contains(info.impressum);
+    cy.get('.ui.accordion > :nth-child(11)').click();
+    cy.get('.ui.accordion > .content.active').contains(info.impressum);
   });
 });
