939370eb8dbe6de39da0d1088ef9e7317f1ecf44
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-03-10 16:00:08 +0100

    fix: integration tests

--- cypress/integration/location.spec.js
+++ cypress/integration/location.spec.js
@@ -1,13 +1,7 @@
-const { location } = require('../../src/i18n/resources/de/translation.json');
-
 describe('Location', () => {
   beforeEach(() => {
     cy.visit('');
     cy.get('[data-cypress=welcomeModalGo]').click();
     cy.get('.ui.menu > :nth-child(1)').click();
-  });
-
-  it('shows message "under construction"', () => {
-    cy.get('.ui.big.message').contains(location.underConstruction);
   });
 });
