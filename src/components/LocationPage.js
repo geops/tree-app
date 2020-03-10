e8bae895a4764cb5e4a411ec550292b3a3ec93ad
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-03-10 14:33:36 +0100

    feat: initial location form

--- src/components/LocationPage.js
+++ src/components/LocationPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import LocationForm from './LocationForm';
+import ProjectionMode from './ProjectionMode';
 import Ecogram from './Ecogram';
 
 function LocationPage() {
   return (
     <>
+      <ProjectionMode />
+      <LocationForm />
       <Ecogram />
     </>
   );
