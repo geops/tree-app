2996755af15b00c01c6505f587ef1d61b05c21f6
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-03-13 15:37:31 +0100

    feat: return options for locate result

--- src/locate.js
+++ src/locate.js
@@ -8,16 +8,17 @@ import locations from '../data/locations.json';
  * @returns {object} ...
  */
 function locate(location = {}) {
+  const options = { forestEcoregion: Object.keys(locations) };
   const { forestEcoregion, altitudinalZone } = location;
   let ecogram;
-  if (
-    locations[forestEcoregion] &&
-    locations[forestEcoregion][altitudinalZone]
-  ) {
-    const id = locations[forestEcoregion][altitudinalZone];
-    ecogram = ecograms[id];
+  if (locations[forestEcoregion]) {
+    options.altitudinalZone = Object.keys(locations[forestEcoregion]);
+    if (locations[forestEcoregion][altitudinalZone]) {
+      const id = locations[forestEcoregion][altitudinalZone];
+      ecogram = ecograms[id];
+    }
   }
-  return { ecogram };
+  return { ecogram, options };
 }
 
 export default locate;
