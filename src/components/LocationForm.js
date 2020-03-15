953f1134d06198b6f538a19c68d4222a9ae5aed6
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-03-15 17:28:06 +0100

    feat: use options from locate function

--- src/components/LocationForm.js
+++ src/components/LocationForm.js
@@ -1,5 +1,5 @@
 import { info } from '@geops/tree-lib';
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form } from 'semantic-ui-react';
@@ -8,52 +8,51 @@ import Dropdown from './Dropdown';
 import styles from './LocationForm.module.css';
 import { setFormLocation } from '../store/actions';
 
-const getDropdownOptions = (type, lng, includeCode = false) => {
-  return info(type).map(item => ({
-    key: item.code,
-    text: includeCode ? `${item.code} - ${item[lng]}` : item[lng],
-    value: item.code,
-  }));
-};
+const getDropdownOptions = (type, lng, includeKey = false) => key => ({
+  key,
+  text: includeKey ? `${key} - ${info(type, key)[lng]}` : info(type, key)[lng],
+  value: key,
+});
 
 function LocationForm() {
   const dispatch = useDispatch();
-  const { formLocation, location, mapLocation, projectionMode } = useSelector(
-    state => ({
-      formLocation: state.formLocation,
-      location: state.location,
-      mapLocation: state.mapLocation,
-      projectionMode: state.projectionMode,
-    }),
-  );
+  const {
+    formLocation,
+    locateResult: { options },
+    location,
+    mapLocation,
+    projectionMode,
+  } = useSelector(state => ({
+    formLocation: state.formLocation,
+    locateResult: state.locateResult,
+    location: state.location,
+    mapLocation: state.mapLocation,
+    projectionMode: state.projectionMode,
+  }));
   const { t, i18n } = useTranslation();
-  const forestEcoregions = useMemo(
-    () => getDropdownOptions('forestEcoregion', i18n.language, true),
-    [i18n.language],
-  );
-  const altitudinalZones = useMemo(
-    () => getDropdownOptions('altitudinalZone', i18n.language),
-    [i18n.language],
-  );
   const isDifferent = field => mapLocation[field] !== formLocation[field];
   return (
     <Form className={styles.form}>
-      {projectionMode === 'f' && (
+      {projectionMode === 'f' && options.forestEcoregion && (
         <Dropdown
           clearable={isDifferent('forestEcoregion')}
           label={t('forestEcoregion.label')}
-          options={forestEcoregions}
+          options={options.forestEcoregion.map(
+            getDropdownOptions('forestEcoregion', i18n.language),
+          )}
           onChange={(e, { value: forestEcoregion }) =>
             dispatch(setFormLocation({ forestEcoregion }))
           }
           value={location.forestEcoregion}
         />
       )}
-      {projectionMode === 'f' && (
+      {projectionMode === 'f' && options.altitudinalZone && (
         <Dropdown
           clearable={isDifferent('altitudinalZone')}
           label={t('altitudinalZone.label')}
-          options={altitudinalZones}
+          options={options.altitudinalZone.map(
+            getDropdownOptions('altitudinalZone', i18n.language),
+          )}
           onChange={(e, { value: altitudinalZone }) =>
             dispatch(setFormLocation({ altitudinalZone }))
           }
