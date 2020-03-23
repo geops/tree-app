9bd482c5a8cac82e76333736190b23265db16ba4
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-03-23 15:49:01 +0100

    fix: select first forest type if only one is provided

--- src/components/ForestTypeModal.js
+++ src/components/ForestTypeModal.js
@@ -12,19 +12,23 @@ function ForestTypeModal() {
   const { search } = useLocation();
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const forestTypes = useSelector(state => state.forestTypes);
-  const selectForestType = forestType => {
+  const forestTypes = useSelector((state) => state.forestTypes);
+  const selectForestType = (forestType) => {
     dispatch(setForestTypes([]));
     dispatch(setFormLocation({ forestType }));
     history.push(`/projection${search}`);
   };
 
+  if (forestTypes.length === 1) {
+    selectForestType(forestTypes[0]);
+  }
+
   return (
-    <Modal open={forestTypes.length > 0}>
+    <Modal open={forestTypes.length > 1}>
       <Modal.Header>{t('forestType.select')}</Modal.Header>
       <Modal.Content>
         <Modal.Description>
-          {forestTypes.map(f => (
+          {forestTypes.map((f) => (
             <Button active onClick={() => selectForestType(f)}>
               {f}
             </Button>
