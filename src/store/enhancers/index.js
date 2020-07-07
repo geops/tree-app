179b75a77ab507163ba8afabd2368dd6d5b1aca5
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-07-07 09:43:34 +0200

    fix: avoid missing Redux devtools

--- src/store/enhancers/index.js
+++ src/store/enhancers/index.js
@@ -4,9 +4,11 @@ import querySync from './querySync';
 import projection from './projection';
 
 const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-    trace: true,
-    traceLimit: 25,
-  }) || compose;
+  (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      trace: true,
+      traceLimit: 25,
+    })) ||
+  compose;
 
 export default composeEnhancers(querySync, projection);
