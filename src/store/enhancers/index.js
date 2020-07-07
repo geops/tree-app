d4722727e04bf666acc3a36abfbc1c1a7e414c15
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-07-07 09:38:25 +0200

    chore: add Redux devtools config

--- src/store/enhancers/index.js
+++ src/store/enhancers/index.js
@@ -3,4 +3,10 @@ import { compose } from 'redux';
 import querySync from './querySync';
 import projection from './projection';
 
-export default compose(querySync, projection);
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    trace: true,
+    traceLimit: 25,
+  }) || compose;
+
+export default composeEnhancers(querySync, projection);
