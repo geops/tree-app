6a24df968ab832b806be844025e6c92ee7b97051
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-09-09 09:01:50 +0200

    fix: handle digit keys in CSV translation files

--- src/i18n/convert2json.js
+++ src/i18n/convert2json.js
@@ -13,11 +13,16 @@ const fr = {};
   const records = parse(data);
   records.shift(); // remove header
   await records.forEach(([key, german, french]) => {
-    set(de, key.split('.'), german);
-    set(fr, key.split('.'), french);
+    const keys = key.split('.');
+    // handle digit keys
+    if (keys[keys.length - 1].match(/^\d*$/)) {
+      keys[keys.length - 1] = `"${keys[keys.length - 1]}"`;
+    }
+    set(de, keys, german);
+    set(fr, keys, french);
   });
   const dePath = path.join(__dirname, 'resources', 'de', 'translation.json');
   const frPath = path.join(__dirname, 'resources', 'fr', 'translation.json');
-  await fs.writeFile(dePath, JSON.stringify(de, null, 2));
-  await fs.writeFile(frPath, JSON.stringify(fr, null, 2));
+  await fs.writeFile(dePath, JSON.stringify(de, null, 2).replace(/\\"/g, ''));
+  await fs.writeFile(frPath, JSON.stringify(fr, null, 2).replace(/\\"/g, ''));
 })();
