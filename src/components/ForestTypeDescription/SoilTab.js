da3670bb17309ba1842b8805308a849e6019494c
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-09-11 14:38:38 +0200

    feat: improve forest type description

--- src/components/ForestTypeDescription/SoilTab.js
+++ src/components/ForestTypeDescription/SoilTab.js
@@ -3,20 +3,30 @@ import React from 'react';
 
 import Graininess from './Graininess';
 import Humus from './Humus';
+import Legend from './Legend';
 import SkeletalFraction from './SkeletalFraction';
 import SoilDiagram from './SoilDiagram';
 import SoilWetnessGroundwater from './SoilWetnessGroundwater';
 import SoilWetnessTailwater from './SoilWetnessTailwater';
+import styles from './Diagram.module.css';
 
 function SoilTab({ data }) {
   return (
     <>
+      <Legend />
       <Humus humus={data.humus} humusVariants={data.humusVariants} />
       <SoilDiagram soil={data.soil} soilVariants={data.soilVariants} />
-      <Graininess graininess={data.graininess} rawMaterial={data.rawMaterial} />
-      <SkeletalFraction data={data.skeletalFractionSoilDepth} />
-      <SoilWetnessGroundwater data={data.soilWetnessGroundwater} />
-      <SoilWetnessTailwater data={data.soilWetnessTailwater} />
+      <div className={styles.group}>
+        <Graininess
+          graininess={data.graininess}
+          rawMaterial={data.rawMaterial}
+        />
+        <SkeletalFraction data={data.skeletalFractionSoilDepth} />
+      </div>
+      <div className={styles.group}>
+        <SoilWetnessGroundwater data={data.soilWetnessGroundwater} />
+        <SoilWetnessTailwater data={data.soilWetnessTailwater} />
+      </div>
     </>
   );
 }
