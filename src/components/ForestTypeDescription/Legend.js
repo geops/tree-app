067e72643581a6465134ed4c38d54ac128b1c60c
danji90 <daniel.marsh-hunn@geops.de>
2021-08-25 17:49:09 +0200

    made legend responsive

--- src/components/ForestTypeDescription/Legend.js
+++ src/components/ForestTypeDescription/Legend.js
@@ -1,50 +1,77 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 
 import styles from './Diagram.module.css';
 
-function Legend() {
+function Legend({ title, rare, medium, often }) {
   const { t } = useTranslation();
   return (
-    <svg viewBox="0 0 800 100" className={styles.svg}>
-      <rect
-        x="203.935"
-        y="55.048"
-        width="26.669"
-        height="26.669"
-        className={`${styles.often} ${styles.line}`}
-      />
-      <text x="245.303px" y="74.34px" className={styles.label}>
-        {t('forestTypeDiagram.legend.often')}
-      </text>
-      <rect
-        x="413.409"
-        y="55.048"
-        width="26.669"
-        height="26.669"
-        className={`${styles.medium} ${styles.line}`}
-      />
-      <text x="455.348px" y="73.688px" className={styles.label}>
-        {t('forestTypeDiagram.legend.medium')}
-      </text>
-      <rect
-        x="654.194"
-        y="55.048"
-        width="26.669"
-        height="26.669"
-        className={`${styles.rare} ${styles.line}`}
-      />
-      <text x="695.562px" y="73.496px" className={styles.label}>
-        {t('forestTypeDiagram.legend.rare')}
-      </text>
-      <text x="30.716px" y="31.75px" className={styles.header}>
-        {t('forestTypeDiagram.legend.label')}
-      </text>
-      <text x="32.531px" y="73.373px" className={styles.label}>
-        {t('forestTypeDiagram.legend.distribution')}
-      </text>
-    </svg>
+    <div className={styles.legend}>
+      {title !== false && (
+        <h3>{title || t('forestTypeDiagram.legend.label')}</h3>
+      )}
+      <div
+        className={styles.legenditems}
+        style={{
+          maxWidth: [rare, medium, often].some((val) => val === false)
+            ? 250
+            : 400,
+        }}
+      >
+        {often && (
+          <svg height="30" viewBox="0 0 150 50">
+            <text className={styles.label} x="40" y="18">
+              {t('forestTypeDiagram.legend.often')}
+            </text>
+            <rect
+              width="26.669"
+              height="26.669"
+              className={`${styles.often} ${styles.line}`}
+            />
+          </svg>
+        )}
+        {medium && (
+          <svg height="30" viewBox="0 0 150 50">
+            <text className={styles.label} x="40" y="18">
+              {t('forestTypeDiagram.legend.medium')}
+            </text>
+            <rect
+              width="26.669"
+              height="26.669"
+              className={`${styles.medium} ${styles.line}`}
+            />
+          </svg>
+        )}
+        {rare && (
+          <svg height="30" viewBox="0 0 150 50">
+            <text className={styles.label} x="40" y="18">
+              {t('forestTypeDiagram.legend.rare')}
+            </text>
+            <rect
+              width="26.669"
+              height="26.669"
+              className={`${styles.rare} ${styles.line}`}
+            />
+          </svg>
+        )}
+      </div>
+    </div>
   );
 }
+
+Legend.propTypes = {
+  rare: PropTypes.bool,
+  medium: PropTypes.bool,
+  often: PropTypes.bool,
+  title: PropTypes.oneOfType(PropTypes.string, PropTypes.bool),
+};
+
+Legend.defaultProps = {
+  rare: true,
+  medium: true,
+  often: true,
+  title: undefined,
+};
 
 export default Legend;
