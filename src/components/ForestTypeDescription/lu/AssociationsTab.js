0f2f34a22dc3aae3c364f2828afdf4484cf5d1e1
danji90 <daniel.marsh-hunn@geops.de>
2021-10-04 16:53:05 +0200

    chore: cleaning

--- src/components/ForestTypeDescription/lu/AssociationsTab.js
+++ src/components/ForestTypeDescription/lu/AssociationsTab.js
@@ -24,8 +24,6 @@ function AssociationsTab({ associationGroupCode }) {
     (type) => type.associationGroupCode === associationGroupCode,
   );
 
-  console.log(forestSubTypes);
-
   return (
     <>
       <h3>
