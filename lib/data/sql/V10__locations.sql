f8915dbc69fb51991ef9bbdf4b5f40913b330e31
danji90 <daniel.marsh-hunn@geops.de>
2022-05-09 14:53:29 +0200

    chore: included forest ecoregions in locations sql script

--- lib/data/sql/V10__locations.sql
+++ lib/data/sql/V10__locations.sql
@@ -34,16 +34,16 @@ COPY
                                  l.y,
                                  ft.forest_type_code,
                                  tft.forest_type_code AS transition_forest_type_code,
-                              --    azt.code AS altitudinal_zones_1995_code,
-                              --    azm.code AS altitudinal_zones_2085_less_dry_code,
-                              --    aze.code AS altitudinal_zones_2085_dry_code,
+                                 azt.code AS altitudinal_zones_1995_code,
+                                 azm.code AS altitudinal_zones_2085_less_dry_code,
+                                 aze.code AS altitudinal_zones_2085_dry_code,
                                  fe.code AS forest_ecoregion_code
      FROM l
      LEFT JOIN locations_custom_forest_type ft ON ft.custom_forest_type_code = l.custom_forest_type_code
      LEFT JOIN locations_custom_forest_type tft ON tft.custom_forest_type_code = l.custom_transition_forest_type_code
      LEFT JOIN forest_ecoregions fe ON ST_Intersects(l.geom, ST_Transform(fe.geom, 3857))
-     -- LEFT JOIN altitudinal_zones_1995_export_mat azt ON ST_Intersects(l.geom, azt.geometry)
-     -- LEFT JOIN altitudinal_zones_2085_dry_export_mat aze ON ST_Intersects(l.geom, aze.geometry)
-     -- LEFT JOIN altitudinal_zones_2085_less_dry_export_mat azm ON ST_Intersects(l.geom, azm.geometry)
+     LEFT JOIN altitudinal_zones_1995_export_mat azt ON ST_Intersects(l.geom, azt.geometry)
+     LEFT JOIN altitudinal_zones_2085_dry_export_mat aze ON ST_Intersects(l.geom, aze.geometry)
+     LEFT JOIN altitudinal_zones_2085_less_dry_export_mat azm ON ST_Intersects(l.geom, azm.geometry)
      ) TO '/data/locations/export.csv'
 DELIMITER ';' CSV HEADER;
