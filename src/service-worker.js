0aa0608f83e9af01ce7567e558f1861cdcac39cb
danji90 <daniel.marsh-hunn@geops.com>
2022-09-12 11:40:57 +0200

    chore: fix tile version

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,7 +11,7 @@ import { clientsClaim } from 'workbox-core';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 
-const CACHE_NAME = 'tree-app-tiles-v7';
+const CACHE_NAME = 'tree-app-tiles-v8';
 const OLD_CACHES = [
   'tree-app-tiles-v1',
   'tree-app-tiles-v2',
@@ -20,6 +20,7 @@ const OLD_CACHES = [
   'tree-app-tiles-v5',
   'tree-app-tiles-v6',
   'tree-app-tiles-v7',
+  'tree-app-tiles-v8',
 ];
 const { REACT_APP_VECTOR_TILES_ENDPOINT: endpoint } = process.env;
 
