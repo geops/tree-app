b2fc0d1e6955845e08782719bf305d1782976702
danji90 <daniel.marsh-hunn@geops.com>
2023-01-26 13:31:32 +0100

    chore: clean matomo code

--- src/components/MatomoTracker.js
+++ src/components/MatomoTracker.js
@@ -2,9 +2,6 @@ import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useMatomo } from '@datapunt/matomo-tracker-react';
 
-const LS_MATOMO_USER_SESSION_TIMER = 'matomo_user_session_timer';
-const LS_MATOMO_TOPIC_VISITED = 'matomo_topic_visited';
-
 function MatomoTracker() {
   const { trackPageView, pushInstruction } = useMatomo();
   const consentGiven = useSelector((state) => state.consentGiven);
@@ -17,10 +14,6 @@ function MatomoTracker() {
 
     pushInstruction('setConsentGiven');
     trackPageView();
-
-    // Start the clock to avoid duplicate 'User topic change' event tracking.
-    localStorage?.setItem(LS_MATOMO_USER_SESSION_TIMER, Date.now());
-    localStorage?.setItem(LS_MATOMO_TOPIC_VISITED, '');
   }, [consentGiven, trackPageView, pushInstruction]);
 
   return null;
