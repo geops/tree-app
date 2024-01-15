cd3cd8302780df7f1cdcdcb94206d31d388a895d
danji90 <daniel.marsh-hunn@geops.com>
2024-01-15 18:59:43 +0100

    chore: try with chromeWebSecurity: false

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   projectId: '81oge9',
   e2e: {
     baseUrl: 'http://localhost:3000',
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
