ed8803997ed9b343a16a1c940d0b4f6f06f9406d
danji90 <daniel.marsh-hunn@geops.com>
2024-01-16 09:40:31 +0100

    chore: remove setupNodeEvents

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,5 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     chromeWebSecurity: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
