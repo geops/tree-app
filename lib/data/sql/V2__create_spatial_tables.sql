962fc9dfa61f58c195b594b6d8403b06af869bac
ElkeErhardt <52400389+ElkeErhardt@users.noreply.github.com>
2024-02-20 13:15:07 +0100

    fix(SO): update SQL for SO altitudinal zones, upgrade tile and app versions (#685)
    * Update V2__create_spatial_tables.sql
    
    * Update V8__export_spatial.sql
    
    * chore: update tile version to 17
    
    * chore: upgrade app version to 2.6.3
    
    ---------
    
    Co-authored-by: danji90 <daniel.marsh-hunn@geops.com>

--- lib/data/sql/V2__create_spatial_tables.sql
+++ lib/data/sql/V2__create_spatial_tables.sql
@@ -362,7 +362,8 @@ CREATE TABLE "forest_types_so" (gid serial,
 "stan_nais" varchar(254),
 "grunnheit" varchar(254),
 "hs" varchar(20),
-"hs_code" float8);
+"hs_code" float8,
+"hsue_code" float8);
 
 ALTER TABLE "forest_types_so" ADD PRIMARY KEY (gid);
 SELECT AddGeometryColumn('','forest_types_so','geom','0','MULTIPOLYGON',2);
