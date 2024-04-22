2c3a9afbdbeb935160086a824772513c735cb439
danji90 <daniel.marsh-hunn@geops.com>
2024-04-22 17:14:11 +0200

    chore: add buttons for cantonal forest types in ProjectionResult

--- src/components/TabFooter.js
+++ src/components/TabFooter.js
@@ -1,77 +1,63 @@
-import React, { useMemo } from 'react'
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
-import { info, utils } from '@geops/tree-lib';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ExportButton from './ExportButton';
-import styles from './ProjectionResult.module.css';
+import Button from './Button';
+import styles from './TabFooter.module.css';
+import getCantonalForestTypes from '../utils/getCantonalForestType';
+import { setForestTypeDescription } from '../store/actions';
 
 function TabFooter(props) {
-    const { t } = useTranslation()
-    const { onExport, forestType, scenario } = props;
-    const activeProfile = useSelector((state) => state.activeProfile);
-    const ftMapping = useMemo(() => {
-        try {
-          return utils().getMapping('forestTypes', activeProfile);
-        } catch (error) {
-          console.log(`No forest type mapping found for ${activeProfile}`, error);
-          return null;
-        }
-    }, [activeProfile]);
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const { onExport, forestType, isFuture } = props;
+  const activeProfile = useSelector((state) => state.activeProfile);
 
+  const cantonalForestTypes = useMemo(
+    () => getCantonalForestTypes(forestType, activeProfile, isFuture),
+    [forestType, activeProfile, isFuture],
+  );
 
-    // const cantonalForestTypes = useMemo(() => {
-    //     if (!ftMapping || !forestType) return [];
-    //     const mappedFts = ftMapping[forestType];
-    //     const forestTypes = mappedFts.reduce((allFts, ft) => {
-    //       let ftInfo;
-    //       try {
-    //         ftInfo = info('forestType', ft, activeProfile);
-    //         console.log('ftInfo', ftInfo);
-    //         forestTypes.push(ftInfo);
-    //       } catch (error) {
-    //         console.log(`No forest type found for ${ft} in ${activeProfile}`, error);
-    //       }
-    //       try {
-    //         ftInfo = info('forestType', ft);
-    //         console.log('ftInfo', ftInfo);
-    //         forestTypes.push(ftInfo);
-    //       } catch (error) {
-    //         console.log(`No forest type found for ${ft} in ch`, error);
-    //       }
-    //       return ftInfo ? [...allFts, ftInfo] : allFts
-    //     }, [])
-    //     return forestTypes;
-    //   }, [forestType, ftMapping, activeProfile]);
-    // console.log(cantonalForestTypes);
+  if (!onExport && !cantonalForestTypes?.length) return null;
 
-    // if (!onExport && !cantonalForestTypes?.length) return null
-
-    return (
-      <div className={styles.exportButtonWrapper}>
-        {onExport && (
-          <ExportButton
-            onClick={onExport}
-            className={`${styles.exportButton}`}
-          >
-            {t('export.exportRecommendation')}
-          </ExportButton>
-        )}
-        {}
-      </div>
-    )
+  return (
+    <div className={styles.tabFooter}>
+      {cantonalForestTypes?.length ? (
+        <div className={styles.cantonalForestTypes}>
+          {cantonalForestTypes.map((ft) => (
+            <Button
+              basic
+              key={ft.code}
+              active
+              compact
+              className={styles.cantonalForestTypeButton}
+              onClick={() => dispatch(setForestTypeDescription(ft))}
+            >
+              {ft.code}
+            </Button>
+          ))}
+        </div>
+      ) : null}
+      {onExport && (
+        <ExportButton onClick={onExport} className={`${styles.exportButton}`}>
+          {t('export.exportRecommendation')}
+        </ExportButton>
+      )}
+    </div>
+  );
 }
 
 TabFooter.propTypes = {
   onExport: PropTypes.func,
   forestType: PropTypes.string,
-  scenario: PropTypes.string,
-}
+  isFuture: PropTypes.bool,
+};
 
 TabFooter.defaultProps = {
-    onExport: null,
-    forestType: null,
-    scenario: null,
-}
+  onExport: null,
+  forestType: null,
+  isFuture: false,
+};
 
-export default TabFooter
+export default TabFooter;
