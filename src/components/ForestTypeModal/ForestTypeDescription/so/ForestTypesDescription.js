b36cf4f3b39c6565e670c67d68fdd76531317aa5
Friedjoff Trautwein <friedjoff.trautwein@geops.com>
2024-04-24 12:54:18 +0200

    feat(Solothurn): use react-pdf library to render SO forest type descriptions

--- src/components/ForestTypeModal/ForestTypeDescription/so/ForestTypesDescription.js
+++ src/components/ForestTypeModal/ForestTypeDescription/so/ForestTypesDescription.js
@@ -1,17 +1,46 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 
-const style = { height: '70vh' };
+import Button from '../../../Button';
+
+const containerStyle = { margin: '-1.5rem', position: 'relative' };
+const buttonStyle = { position: 'absolute', top: 10, right: 10, zIndex: 999 };
 
 function ForestTypeDescription({ code }) {
   const name = code.replace('*', 'stern');
+  const containerRef = useRef(null);
+  const [numPages, setNumPages] = useState(null);
+  const { t } = useTranslation();
+
+  const onDocumentLoadSuccess = ({ numPages: nextNumPages }) => {
+    setNumPages(nextNumPages);
+  };
+
   return (
-    <embed
-      type="application/pdf"
-      src={`https://so-data.tree-app.ch/forest-types/${name}.pdf`}
-      width="100%"
-      style={style}
-    />
+    <div style={containerStyle} ref={containerRef}>
+      <Button
+        active
+        style={buttonStyle}
+        href={`https://so-data.tree-app.ch/forest-types/${name}.pdf`}
+        target="so-data"
+      >
+        {t('export.export')}
+      </Button>
+      <Document
+        file={`https://so-data.tree-app.ch/forest-types/${name}.pdf`}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        {Array.from(new Array(numPages), (_, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+            width={containerRef.current?.clientWidth}
+          />
+        ))}
+      </Document>
+    </div>
   );
 }
 
