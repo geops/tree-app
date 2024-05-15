ea231b37c51235eec3d2dc361c03d6c4740e0c2e
danji90 <daniel.marsh-hunn@geops.com>
2024-05-15 09:21:11 +0200

    chore: add abort controller to PDF fetch

--- src/components/ForestTypeModal/ForestTypeDescription/so/useHasPdf.js
+++ src/components/ForestTypeModal/ForestTypeDescription/so/useHasPdf.js
@@ -1,25 +1,33 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
+let abortCtrl = new AbortController();
+
 const useHasPdf = (forestType) => {
   const activeProfile = useSelector((state) => state.activeProfile);
   const [hasPdf, setHasPdf] = useState(false);
   useEffect(() => {
-    if (!forestType || activeProfile !== 'so') return;
-    const fetchPDF = async () => {
-      const url = `https://so-data.tree-app.ch/forest-types/${forestType.replace(
-        '*',
-        'stern',
-      )}.pdf`;
-      fetch(url)
-        .then((res) => setHasPdf(res.ok === true && res.status === 200))
-        .catch((err) => {
-          // eslint-disable-next-line no-console
-          console.warn(`No PDF found for ${forestType}`, err);
-          setHasPdf(false);
-        });
+    if (forestType && activeProfile === 'so') {
+      abortCtrl.abort()
+      abortCtrl = new AbortController();
+      const fetchPDF = async () => {
+        const url = `https://so-data.tree-app.ch/forest-types/${forestType.replace(
+          '*',
+          'stern',
+        )}.pdf`;
+        fetch(url, { signal: abortCtrl.signal })
+          .then((res) => setHasPdf(res.ok === true && res.status === 200))
+          .catch((err) => {
+            // eslint-disable-next-line no-console
+            console.warn(`No PDF found for ${forestType}`, err);
+            setHasPdf(false);
+          });
+      };
+      fetchPDF();
+    }
+    return () => {
+      abortCtrl.abort()
     };
-    fetchPDF();
   }, [forestType, activeProfile]);
   return hasPdf;
 };
