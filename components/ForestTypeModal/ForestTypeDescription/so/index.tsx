84c087d040c1f8604eed628799ebea9f15e94c45
danji90 <daniel.marsh-hunn@geops.com>
2025-07-23 09:01:09 +0200

    chore: fix SO FTD on load

--- components/ForestTypeModal/ForestTypeDescription/so/index.tsx
+++ components/ForestTypeModal/ForestTypeDescription/so/index.tsx
@@ -6,12 +6,14 @@ pdfjs.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.mjs"; // we host this sta
 
 import PdfViewer from "@/components/PdfViewer";
 import { buttonStyles, primaryStyles } from "@/components/ui/Button";
+import { useModalContext } from "@/components/ui/Modal";
 
 const soPdfEndpoint = process.env.NEXT_PUBLIC_SO_PDF_ENDPOINT;
 
 function ForestTypeDescription({ code }: { code: string }) {
   const name = code?.replace("*", "stern");
   const { t } = useTranslation();
+  const { isOpen } = useModalContext();
 
   return (
     <div className="flex flex-col gap-2">
@@ -22,7 +24,8 @@ function ForestTypeDescription({ code }: { code: string }) {
       >
         {t("export.exportForestTypeDescription")}
       </a>
-      <PdfViewer href={`${soPdfEndpoint}/${name}.pdf`} />
+      {/* We react to is open to rerender the component */}
+      {isOpen ? <PdfViewer href={`${soPdfEndpoint}/${name}.pdf`} /> : null}
     </div>
   );
 }
