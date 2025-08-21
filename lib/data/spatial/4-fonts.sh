be1ed6ffdcd04f9d4814949c25510031bebd0f56
danji90 <daniel.marsh-hunn@geops.com>
2025-08-21 16:29:33 +0200

    chore: fix font output path

--- lib/data/spatial/4-fonts.sh
+++ lib/data/spatial/4-fonts.sh
@@ -1,4 +1,4 @@
 yarn run generate-mapbox-gl-fonts
 mkdir -p data/spatial/tiles/fonts
-cp -r _output/Roboto\ Regular data/spatial/tiles/fonts/Roboto\ Regular
+cp -r _output/Roboto\ Regular data/spatial/tree-app-tiles/tiles/fonts/Roboto\ Regular
 rm -r _output
