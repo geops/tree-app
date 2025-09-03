e47b73e0ff179499929df08263669d5ac39a19be
danji90 <daniel.marsh-hunn@geops.com>
2025-09-03 13:55:24 +0200

    chore: fix tile build process

--- lib/data/spatial/3-tile.sh
+++ lib/data/spatial/3-tile.sh
@@ -4,7 +4,7 @@ _tile () {
     local MAX_ZOOM=${3:-14}  # Default to 14 if not provided
     local MIN_ZOOM=${4:-0}   # Default to 0 if not provided
     echo "Tiling ${TARGET} ..."
-    tippecanoe --no-tile-size-limit --detect-shared-borders --no-feature-limit --include=code --include=code_style --include=code_vd --include=info_vd --include=code_so --minimum-zoom=${MIN_ZOOM} --maximum-zoom=${MAX_ZOOM} -s EPSG:3857 --layer ${LAYER_NAME} --force -o "/data/spatial/tiles/${TARGET}.mbtiles" /data/spatial/export/${TARGET}.geojson
+    tippecanoe --no-tile-size-limit --detect-shared-borders --no-feature-limit --include=code --include=code_style --include=code_vd --include=info_vd --include=code_so --minimum-zoom=${MIN_ZOOM} --maximum-zoom=${MAX_ZOOM} -s EPSG:3857 --layer ${LAYER_NAME} --force -o "/data/spatial/tree-app-tiles/tiles/${TARGET}.mbtiles" /data/spatial/export/${TARGET}.geojson
 }
 
 start=`date +%s`
@@ -13,8 +13,8 @@ _tile "altitudinal_zones_2085_dry" "altitudinal_zones_2085_dry"
 _tile "altitudinal_zones_2085_less_dry" "altitudinal_zones_2085_less_dry"
 _tile "cantonal_boundaries" "cantonal_boundaries"
 _tile "forest_ecoregions" "forest_ecoregions"
-_tile "forest_types" "forest_types" 14 11
-_tile "forest_types_union" "forest_types" 10 0
+_tile "forest_types" "forest_types" 14 12
+_tile "forest_types_union" "forest_types" 11 0
 _tile "silver_fir_areas" "silver_fir_areas"
 
 echo "Joining tiles ..."
