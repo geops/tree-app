16b6b4bde982a968a914abf31b4bdc35dfe1cbda
Friedjoff Trautwein <friedjoff.trautwein@geops.com>
2025-11-03 11:26:51 +0100

    reactivate original code

--- sw.ts
+++ sw.ts
@@ -19,10 +19,6 @@ const serwist = new Serwist({
   clientsClaim: true,
   navigationPreload: true,
   precacheEntries: self.__SW_MANIFEST,
-  // precacheOptions: {
-  //   // Ignore all URL parameters.
-  //   ignoreURLParametersMatching: [/.*/],
-  // },
   runtimeCaching: [
     ...defaultCache,
     {
@@ -40,40 +36,40 @@ const treePdfCacheString = "tree-data-v"; // IMPORTANT: This string should NEVER
 const currentTreePdfVersion = 1; // Current Tree PDF version, needs to be increased every time new PDFs are deployed
 const TREE_CACHE_NAME = `${treePdfCacheString}${currentTreePdfVersion}`; // Cache name for Tree profile data
 
-// // Create an array of 'tree-data-v[1 - currentVersion]' strings for the caches to be removed
-// const OLD_TREE_PDF_CACHES = Array.from(Array(currentTreePdfVersion).keys()).map(
-//   (version) => `${treePdfCacheString}${version}`,
-// );
+// Create an array of 'tree-data-v[1 - currentVersion]' strings for the caches to be removed
+const OLD_TREE_PDF_CACHES = Array.from(Array(currentTreePdfVersion).keys()).map(
+  (version) => `${treePdfCacheString}${version}`,
+);
 
 const soPdfCacheString = "so-data-v"; // IMPORTANT: This string should NEVER be changed, otherwise the old caches will not be identifyable anymore
 const currentSoPdfVersion = 3; // Current SO PDF version, needs to be increased every time new PDFs are deployed
 const SO_CACHE_NAME = `${soPdfCacheString}${currentSoPdfVersion}`; // Cache name for SO profile data
 
-// // Create an array of 'so-data-v[1 - currentVersion]' strings for the caches to be removed
-// const OLD_SO_PDF_CACHES = Array.from(Array(currentSoPdfVersion).keys()).map(
-//   (version) => `${soPdfCacheString}${version}`,
-// );
+// Create an array of 'so-data-v[1 - currentVersion]' strings for the caches to be removed
+const OLD_SO_PDF_CACHES = Array.from(Array(currentSoPdfVersion).keys()).map(
+  (version) => `${soPdfCacheString}${version}`,
+);
 
 const tileCacheString = "tree-app-tiles-v"; // IMPORTANT: This string should NEVER be changed, otherwise the old caches will not be identifyable anymore
 const currentTileVersion = 25; // Current tile version, needs to be increased every time new tiles are deployed
 const TILE_CACHE_NAME = `${tileCacheString}${currentTileVersion}`;
 
-// // Create an array of 'tree-app-tiles-v[1 - currentVersion]' strings for the caches to be removed
-// const OLD_TILE_CACHES = Array.from(Array(currentTileVersion).keys()).map(
-//   (version) => `${tileCacheString}${version}`,
-// );
+// Create an array of 'tree-app-tiles-v[1 - currentVersion]' strings for the caches to be removed
+const OLD_TILE_CACHES = Array.from(Array(currentTileVersion).keys()).map(
+  (version) => `${tileCacheString}${version}`,
+);
 
-// // This allows the web app to trigger skipWaiting via
-// // registration.waiting.postMessage({type: 'SKIP_WAITING'})
-// self.addEventListener("message", (event) => {
-//   if (event.data && (event.data as MessageEvent).type === "SKIP_WAITING") {
-//     void self.skipWaiting();
-//   }
-// });
+// This allows the web app to trigger skipWaiting via
+// registration.waiting.postMessage({type: 'SKIP_WAITING'})
+self.addEventListener("message", (event) => {
+  if (event.data && (event.data as MessageEvent).type === "SKIP_WAITING") {
+    void self.skipWaiting();
+  }
+});
 
-// [...OLD_SO_PDF_CACHES, ...OLD_TILE_CACHES, ...OLD_TREE_PDF_CACHES].forEach(
-//   (OLD_CACHE) => void caches.delete(OLD_CACHE),
-// );
+[...OLD_SO_PDF_CACHES, ...OLD_TILE_CACHES, ...OLD_TREE_PDF_CACHES].forEach(
+  (OLD_CACHE) => void caches.delete(OLD_CACHE),
+);
 
 self.addEventListener("install", (event) => {
   const soPdfEndpoint = process.env.NEXT_PUBLIC_SO_PDF_ENDPOINT;
