fd9442dd4854816c97297ceac53a8488a52e2c5b
danji90 <daniel.marsh-hunn@geops.com>
2026-02-09 16:40:09 +0100

    chore: prevent app from crashing

--- lib/src/TreeClient/index.ts
+++ lib/src/TreeClient/index.ts
@@ -164,7 +164,7 @@ class TreeClient {
     const { data: lists } = this.executeQuery<Recommendation>(
       `select * from ${recommendationTableName} where foresttype = '${forestType}'`,
     );
-    return [...(lists?.[0].recommendations ?? emptyLists)];
+    return [...(lists?.[0]?.recommendations ?? emptyLists)];
   }
 
   public getSilverFirAreaByCode(
