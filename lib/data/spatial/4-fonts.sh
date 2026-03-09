ef1556301ebd8f1a234498915bf6c0fc7c78aa2f
danji90 <daniel.marsh-hunn@geops.com>
2026-03-09 13:54:55 +0100

    fix(tiles): rebuild tiles with FR and SZ data, upgrade tile version

--- lib/data/spatial/4-fonts.sh
+++ lib/data/spatial/4-fonts.sh
@@ -1,4 +1,7 @@
-yarn run generate-mapbox-gl-fonts
+source ~/.nvm/nvm.sh
+nvm use 16  # generate-mapbox-gl-fonts needs node 16
+
+yarn --ignore-engines run generate-mapbox-gl-fonts
 mkdir -p data/spatial/tiles/fonts
 cp -r _output/Roboto\ Regular data/spatial/tree-app-tiles/tiles/fonts/Roboto\ Regular
 rm -r _output
